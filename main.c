c344923537fcf5d185446c8b187f94b8efeca665
Ivan Kireev <ivkireev@yandex.ru>
2017-05-23 02:38:08 +0300

    #1. Empty VS project

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * tomate01.c
+ *
+ * Created: 23.05.2017 2:29:30
+ * Author : ivkir
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
