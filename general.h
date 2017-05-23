ca11d78ed7accfb3bddbe3b97cd2fd0aff7319cc
Ivan Kireev <ivkireev@yandex.ru>
2017-05-23 23:21:47 +0300

    main finished

---
+++ general.h
@@ -0,0 +1,17 @@
+/*
+ * general.h
+ *
+ * Created: 23.05.2017 22:38:10
+ *  Author: Ivan Kireev
+ */ 
+
+
+#ifndef GENERAL_H_
+#define GENERAL_H_
+
+void general_init(void);
+
+
+
+
+#endif /* GENERAL_H_ */
--- main.c
+++ main.c
@@ -2,17 +2,42 @@
  * tomate01.c
  *
  * Created: 23.05.2017 2:29:30
- * Author : ivkir
+ * Author : Ivan Kireev
  */ 
 
 #include <avr/io.h>
 
+#define F_CPU 7372800  // 7.3728 MHz
+#include <util/delay.h>
+
+#include "general.h"
+#include "led.h"
 
 int main(void)
 {
-    /* Replace with your application code */
+	//initialization
+	general_init();
+	led_init();
+
+	//blink 2 times per 1 second on start
+	_delay_ms(1000);
+	led_on(0);
+	_delay_ms(1000);
+	led_off(0);
+	_delay_ms(1000);
+	led_on(0);
+	_delay_ms(1000);
+	led_off(0);
+	
+    //main loop
     while (1) 
     {
+		//blink on 0.3 second in minute
+		_delay_ms(59700);
+		led_on(0);
+		_delay_ms(300);
+		led_off(0);
+		
     }
 }
 
